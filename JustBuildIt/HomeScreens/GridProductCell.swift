aeb747b242d3efcced9048dee9b1f82dc888f5cd
JCB17373 <jcb17373@gmail.com>
2024-11-29 14:22:52 +0530

    Displayed the data from the API at homescreen

---
+++ JustBuildIt/HomeScreens/GridProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  GridProductCell.swift
+//  JustBuildIt
+//
+//  Created by Aman Bhatt on 29/11/24.
+//
+
+import SwiftUI
+
+struct GridProductCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GridProductCell()
+}
